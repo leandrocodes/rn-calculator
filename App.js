d3b8b2844cfbfcf1929d92b200018663611030b8
leandrocodes <leandrovianacodes@gmail.com>
2021-02-26 09:43:26 -0400

    display component

--- App.js
+++ App.js
@@ -1,20 +1,6 @@
-import { StatusBar } from 'expo-status-bar'
-import Constants from 'expo-constants';
 import React from 'react'
-import { SafeAreaView, StyleSheet } from 'react-native'
 import Home from './src/screens/Home'
 
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Home />
-    </SafeAreaView>
-  )
+  return <Home />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: Constants.statusBarHeight
-  }
-})
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,32 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Text, View, StyleSheet } from 'react-native'
+import Constants from 'expo-constants';
 import Button from '../components/Button'
+import Display from '../components/Display'
+
+export default () => {
+  const [displayValue, setDisplayValue] = useState(0)
+  return (
+    <View style={styles.container}>
+      <Display value={displayValue} />
+      <View style={styles.buttons}>
+        <Button label='AC'></Button>
+        <Button label='/'></Button>
+        <Button label='7'></Button>
+        <Button label='8'></Button>
+        <Button label='9'></Button>
+        <Button label='*'></Button>
+        <Button label='4'></Button>
+        <Button label='5'></Button>
+        <Button label='6'></Button>
+        <Button label='-'></Button>
+        <Button label='1'></Button>
+        <Button label='2'></Button>
+        <Button label='3'></Button>
+        <Button label='+'></Button>
+        <Button label='0'></Button>
+        <Button label='.'></Button>
+        <Button label='='></Button>
+      </View>
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   buttons: {
     flexDirection: 'row',
     flexWrap: 'wrap'
+  },
+
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight
   }
 })
-
-export default () => (
-  <View style={styles.buttons}>
-    <Button label="AC"></Button>
-    <Button label="/"></Button>
-    <Button label="7"></Button>
-    <Button label="8"></Button>
-    <Button label="9"></Button>
-    <Button label="*"></Button>
-    <Button label="4"></Button>
-    <Button label="5"></Button>
-    <Button label="6"></Button>
-    <Button label="-"></Button>
-    <Button label="1"></Button>
-    <Button label="2"></Button>
-    <Button label="3"></Button>
-    <Button label="+"></Button>
-    <Button label="0"></Button>
-    <Button label="."></Button>
-    <Button label="="></Button>
-  </View>
-)
